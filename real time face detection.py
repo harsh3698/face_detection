6a35a9f366b961acc51cb5f88d3f1839e3d97dfb
harsh3698 <42904605+harsh3698@users.noreply.github.com>
2019-09-28 11:08:42 +0530

    Update real time face detection.py
    Added description

--- real time face detection.py
+++ real time face detection.py
@@ -15,23 +15,24 @@ while True:
     k = cv2.waitKey(1)
     faces = face_cascade.detectMultiScale(
             gray,
-            scaleFactor = 1.5,
+            scaleFactor = 1.5,                             
             minNeighbors = 5,
             minSize = (30, 30),
             flags = cv2.CASCADE_SCALE_IMAGE
             )
+    # Assuming the resolution of camera is 640*480
     cv2.rectangle(image, (200, 120), (440,360), (255, 255, 255), 2)
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x,y), (x+w, y+h), (255, 255, 255), 2)
     cv2.imshow('FaceDetection', image)
-    if k%256 == 27:
+    if k%256 == 27:   # Press esc to exit
         break
     elif 210<x<240 and 130<y<160 and 400<x+w<430 and 320<y+h<350: # It takes a single image if the face comes in the region
-        if i == 1:
+        if i == 1:                  
             break
         else:
-            ima = "{}.jpg".format(i)                # Image saved in directory
-            cv2.imwrite(ima, image[y:y+h, x:x+w])
+            ima = "{}.jpg".format(i)                
+            cv2.imwrite(ima, image[y:y+h, x:x+w])     # Image saved in directory
             img_counter += 1
             i+=1
 camera.release()
