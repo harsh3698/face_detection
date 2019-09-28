dd1ef1c642e62d7000b2d734252d7df6c3be6973
harsh3698 <42904605+harsh3698@users.noreply.github.com>
2019-09-28 11:04:55 +0530

    Update face detect.py
    Added description

--- face detect.py
+++ face detect.py
@@ -7,7 +7,7 @@ Created on Fri Sep 27 12:48:38 2019
 
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)      # 1 for external camera
 img_counter = 0
 i = 0
 while True:
@@ -21,18 +21,20 @@ while True:
             minSize = (30, 30),
             flags = cv2.CASCADE_SCALE_IMAGE
             )
+    # Assuming the resolution of the camera is 640*480
     cv2.rectangle(image, (200, 120), (440,360), (255, 255, 255), 2)
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x,y), (x+w, y+h), (255, 255, 255), 2)
     cv2.imshow('FaceDetection', image)
-    if k%256 == 27:
+    if k%256 == 27:     #press esc to exit
         break
+    # Takes multiple images if the detected face come sin this cooerdinates
     elif 210<x<240 and 130<y<160 and 400<x+w<430 and 320<y+h<350:
         print(x, y, x+w, y+h)
         img_name = "facedetect_webcam_{}.jpg".format(img_counter)
-        cv2.imwrite(img_name, image)
+        cv2.imwrite(img_name, image)   # Saves the screenshot from the video
         ima = "{}.jpg".format(i)
-        cv2.imwrite(ima, image[y:y+h, x:x+w])
+        cv2.imwrite(ima, image[y:y+h, x:x+w])    # Saves region of face 
         print("{} written!".format(img_name))
         img_counter += 1
         i+=1
